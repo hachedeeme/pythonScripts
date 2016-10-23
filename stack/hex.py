6e1e5faf9394ed404e7c00bee52cce9a0355024a
hachedeeme <hdmaidana@gmail.com>
2016-10-23 03:12:39 -0300

    Se agrego documentacion al modulo hex.py de stack.

--- stack/hex.py
+++ stack/hex.py
@@ -6,10 +6,23 @@ def to_hex(number, limit=4):
   bits = limit - int(math.log(number, 16) + 1)
   # Obtengo el relleno que puede necesitar el numero para llegar al limite.
   fill = '0' * bits
-  return '0x' + (fill + '{:x}'.format(number))[-limit:]
+  return '0x' + (fill + '{:x}'.format(number))[-limit:].upper()
 
 def hex_operation(func):
   return lambda hexX, hexY: to_hex(func(hexX, hexY))
+
+# ============================#
+# === Operaciones en Hexa === #
+# ============================#
+# Estas operaciones pueden recibir tanto numeros decimales 
+# como numeros expresados en hexadecimal.
+# Retorna el resultado de la operacion expresado en hexa de 
+# tipo string.
+# Ejemplos: 
+# - add(1,2) ------------> 0x0003
+# - add(1,0x0003) -------> 0x0004
+# - sub(0x00FF,1) -------> 0x00FE
+# - sub(0x000A,0x0003) --> 0x0007
 
 def add(hexX, hexY):
   return hex_operation(lambda x,y: x+y)(hexX, hexY)
